68665f80821f58630798d91bbae585dcba780b33
Ernes <ernestocarloslimaco@gmail.com>
2023-05-25 19:27:22 -0500

    sea ñadio el ignore

--- owner/views.py
+++ owner/views.py
@@ -2,8 +2,13 @@ from django.shortcuts import render
 
 # Create your views here.
 def owner_list(request):
-    date_context = {
-        'nombre' : 'ernesto carlos',
-    }
+    date_context = [{
 
-    return  render(request,'owner/owner.list.html',date_context)
+        'nombre' : 'ernesto ',
+        'apellido': 'limaco',
+        'edad':17,
+        'dni': 1111111,
+        'pais': 'perusalen',
+    }]
+
+    return  render(request,'owner/owner_list.html',context= {'data':date_context})
